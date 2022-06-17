46e3c7312bdae21e0efaddbe678295e475ffe325
GeorgiGrancharov2233 <107725590+GeorgiGrancharov2233@users.noreply.github.com>
2022-06-18 01:37:47 +0300

    Add files via upload

---
+++ Multiplication_table.java
@@ -0,0 +1,16 @@
+package Basic_Syntax_CondStatements;
+
+import java.util.Scanner;
+
+public class Multiplication_table {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+
+        int n=Integer.parseInt(scanner.nextLine());
+
+        for (int i = 1; i <=10 ; i++) {
+
+            System.out.printf("%d X %d = %d%n",n,i,n*i);
+        }
+    }
+}
